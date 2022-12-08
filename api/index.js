2630e4bb04c075cc677a51ea18bf395cb1e35600
Carlos Eduardo <ceduardo.roriz@gmail.com>
2022-12-08 15:47:55 -0300

    adding a testing route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,18 @@
 import express from "express";
 import { Router } from "express";
 
+import { createRequire } from "module";
+
+
+
+const require = createRequire(import.meta.url)
 const app = express()
 const route = Router();
+
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
 
 
 const PORT = 3003
@@ -18,7 +28,13 @@ app.use(
     }),
 )
 
-app.unsubscribe(express.json())
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+app.use(express.json())
 
 // initial route/endpoint
 route.get('/', (req,res) => {
@@ -28,10 +44,29 @@ route.get('/', (req,res) => {
     })
 })
 
+route.get('/company', (req,res) => {
+    res.json({
+        sucess: true,
+        message:"Here your companies"
+    })
+})
+
+
+
 app.use(route)
 
 // giving te PORT
 
-app.listen(PORT, () => {
-    console.log("Server is running in " + PORT)
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.gdrrb.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    console.log('Successfully connected to MongoDB')
+    app.listen(PORT)                                    //só estou ouvindo a porta caso a API consiga conectar ao banco primeiro
 })
+.catch((err) => console.log(err))
+
+// app.listen(PORT, () => {
+//     console.log("Server is running in " + PORT)
+// })
